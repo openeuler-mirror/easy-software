b6b979f016afe002fae7f9c663d7e397350b24ec
Erotik <1723168479@qq.com>
2024-08-30 09:43:18 +0800

    fix: 软件维护详情仓库、sig筛选，仓库根据选择的sig显示，sig筛选列表去重

--- src/api/api-collaboration.ts
+++ src/api/api-collaboration.ts
@@ -23,9 +23,9 @@ export function getAdminRepos(params: CollaborationRepoT) {
 /**
  * 获取仓库/sig列表
  */
-export function getRepoSigList(permission: string): Promise<{ sigs: string[]; repos: string[] }> {
+export function getRepoSigList(permission: string) {
   const url = `/server/collaboration/${permission}/query/repos`;
-  return request.get<{ data: Record<string, string> }>(url).then((res) => ({ sigs: Object.values(res.data.data), repos: Object.keys(res.data.data) }));
+  return request.get<{ data: Record<string, string> }>(url).then((res) => res.data.data);
 }
 
 // 待我审批
